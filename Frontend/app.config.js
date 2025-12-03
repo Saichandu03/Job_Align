251ef0df952180763e2ed59110fbb1d8d7b3b494
SaiChandu004 <22a91a61d1@technicalhub.io>
2025-12-03 22:27:02 +0530

    feat: add initial app configuration for Expo setup

---
+++ Frontend/app.config.js
@@ -0,0 +1,31 @@
+export default {
+  expo: {
+    name: 'Job_Align',
+    slug: 'job-align',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'light',
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff'
+    },
+    assetBundlePatterns: ['**/*'],
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#ffffff'
+      }
+    },
+    web: {
+      favicon: './assets/favicon.png'
+    },
+    extra: {
+      API_BASE_URL: process.env.EXPO_PUBLIC_API_BASE_URL,
+    }
+  }
+};
